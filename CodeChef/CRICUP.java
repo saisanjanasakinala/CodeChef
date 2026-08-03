4cae6a9a1ea59047af0346c90de2776e28c77b4a
Sai Sanjana Sakinala <saisanjanasakinala@gmail.com>
2026-08-03 20:22:57 +0530

    CodeChef: CRICUP - Accepted Solution

---
+++ CodeChef/CRICUP.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int X=sc.nextInt();
+		    int Y=sc.nextInt();
+		    int D=sc.nextInt();
+		    if(Math.abs(X-Y)<=D){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
