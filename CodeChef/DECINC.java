43f7968f9936c11f6da20d01c7713429022768ec
Sai Sanjana Sakinala <saisanjanasakinala@gmail.com>
2026-08-03 20:45:12 +0530

    CodeChef: DECINC - Accepted Solution

---
+++ CodeChef/DECINC.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		if(N%4==0){
+		    System.out.println(N+1);
+		}
+		else{
+		    System.out.println(N-1);
+		}
+
+	}
+}
