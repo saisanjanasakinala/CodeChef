c11c8fadb8b810b8219909fb76527493a1f0df2a
Sai Sanjana Sakinala <saisanjanasakinala@gmail.com>
2026-08-05 21:10:56 +0530

    CodeChef: CRQDWE30 - Accepted Solution

---
+++ CodeChef/CRQDWE30.java
@@ -0,0 +1,15 @@
+class Codechef {
+    public static void main(String[] args) {
+        // Declare a double variable for total amount in USD
+        double totalAmountUSD = 100.0;
+        double usd=1.2121212121212122;
+
+        // Declare a double variable for the exchange rate
+        double exchangeRate = 82.5; 
+        System.out.println("Total Amount in INR: "+ totalAmountUSD);
+        System.out.println("Exchange Rate (1 USD to INR): "+   exchangeRate);
+        System.out.println("Converted Amount in USD: "+usd);
+
+      
+    }
+}
