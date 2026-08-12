f72d4c8bcf5df31ff2fcff627cd2b018aff06d9f
Sai Sanjana Sakinala <saisanjanasakinala@gmail.com>
2026-08-12 20:21:00 +0530

    CodeChef: JASSIGNMENTS - Accepted Solution

---
+++ CodeChef/JASSIGNMENTS.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-- >0){
+		    int X=sc.nextInt();
+		    if((10-X) >= 3){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
