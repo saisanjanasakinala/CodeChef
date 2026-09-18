30e9572250e7ccf9aa1fad7d92d055159b6ef2b8
Sai Sanjana Sakinala <saisanjanasakinala@gmail.com>
2026-09-18 22:13:23 +0530

    CodeChef: BULLET - Accepted Solution

---
+++ CodeChef/BULLET.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		   int X=sc.nextInt();
+		   int Y=sc.nextInt();
+		   int Z=sc.nextInt();
+		   System.out.println(Math.max(0,Z-(Y/X)));
+		}
+		
+
+	}
+}
